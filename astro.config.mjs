e3239c08b5ca39e843bfbe35c32fa97cd6bb6fe4
szkm330 <skxyue98@outlook.com>
2026-05-23 01:40:39 +0900

    update build.format config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
 
   devToolbar: {
     enabled: false
